5922756882d052609ea2f8c79c3e59115072ed15
v1se <egornechaev78@gmail.com>
2020-02-21 18:23:09 +0300

    Ttt

---
+++ Assets/2Cars/Assets/Scripts/UI/Buttons/HomeButton.cs
@@ -0,0 +1,22 @@
+﻿//  /*********************************************************************************
+//   *********************************************************************************
+//   *********************************************************************************
+//   * Produced by Skard Games										                 *
+//   * Facebook: https://goo.gl/5YSrKw											     *
+//   * Contact me: https://goo.gl/y5awt4								             *
+//   * Developed by Cavit Baturalp Gürdin: https://tr.linkedin.com/in/baturalpgurdin *
+//   *********************************************************************************
+//   *********************************************************************************
+//   *********************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class HomeButton : MonoBehaviour {
+
+    public void OnClickHomeButton()
+    {
+        Managers.Audio.PlayUIClick();
+        Managers.Game.SetState(typeof(MenuState));
+    }
+}
